58e1dabc7f8c4c840b14dac99d3098502f5803b5
ch3rk4 <pety.shurupov@gmail.com>
2025-06-03 13:42:05 +0300

    refresh files

--- config/urls.py
+++ config/urls.py
@@ -3,8 +3,14 @@ URL configuration for django_homework project.
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls')),
 ]
+
+# Настройка для отображения медиафайлов в режиме разработки
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
