9f166c7e098022bac76e84d83b432ed0ad727d99
ch3rk4 <pety.shurupov@gmail.com>
2025-06-09 16:06:39 +0300

    refresh urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,26 +1,17 @@
 from django.urls import path
-from . import views
+from .views import HomeView, ProductDetailView, ContactView
 
-# Пространство имен для приложения catalog
-# Это позволяет нам использовать 'catalog:home' вместо просто 'home'
-# что очень полезно в больших проектах с множеством приложений
+# Пространство имен приложения для избежания конфликтов URL
 app_name = 'catalog'
 
 urlpatterns = [
-    # Главная страница - отображает список всех товаров с пагинацией
-    # Паттерн: / (корневой URL)
-    path('', views.home, name='home'),
+    # Главная страница каталога
+    path('', HomeView.as_view(), name='home'),
 
-    # Страница детального просмотра товара - новое в ДЗ3
-    # Паттерн: /product/1/ где 1 это pk (первичный ключ) товара
-    # <int:pk> означает, что Django ожидает целое число и передаст его как параметр pk
-    path('product/<int:pk>/', views.product_detail, name='product_detail'),
+    # Детальная страница товара
+    # <int:pk> означает, что ожидаем целое число (ID товара)
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
 
-    # Страница контактов - уже была в предыдущих заданиях
-    # Паттерн: /contacts/
-    path('contacts/', views.contacts, name='contacts'),
-
-    # Страница добавления нового товара - дополнительное задание
-    # Паттерн: /add-product/
-    path('add-product/', views.add_product, name='add_product'),
+    # Страница контактов
+    path('contact/', ContactView.as_view(), name='contact'),
 ]
