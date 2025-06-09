1700072391d49d0186fe7b5763bd24bb26f61158
ch3rk4 <pety.shurupov@gmail.com>
2025-06-09 16:10:25 +0300

    refresh urls from config

--- config/urls.py
+++ config/urls.py
@@ -1,16 +1,22 @@
-"""
-URL configuration for django_homework project.
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    # Административная панель Django
     path('admin/', admin.site.urls),
+
+    # Главная страница и каталог товаров
     path('', include('catalog.urls')),
+
+    # Все URL-ы блога начинаются с 'blogs/'
+    # Требование задания - контроллеры должны быть зарегистрированы на адреса blogs/...
+    path('blogs/', include('blog.urls')),
 ]
 
-# Настройка для отображения медиафайлов в режиме разработки
+# В режиме разработки добавляем обслуживание медиафайлов
+# В продакшене это будет делать веб-сервер (nginx, apache)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
